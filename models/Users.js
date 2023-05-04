d154eb7b6612430c5ea96072c4bc468d14071ccd
Long Phan Hoang Le <long.phanhoangle@hitachivantara.com>
2023-05-04 11:35:47 +0700

    add comment

--- models/Users.js
+++ models/Users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: {
+    name: { // The name of the user
         type: String,
         require: true,
         unique: true
